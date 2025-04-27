bf3a42632739e0507a87cf2a7a79d724dce630f5
andrej.cuscak <cuscak@gmail.com>
2025-04-27 23:42:21 +0200

    fiirst endpoint

---
+++ src/test/java/com/polarbookshop/catalog_service/HomeControllerTests.java
@@ -0,0 +1,23 @@
+package com.polarbookshop.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetGreeting() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Welcome to the book catalog!"));
+    }
+}
